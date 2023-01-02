0ce7534dcf01059200b5e4c3fd1b172e3ec85fa3
mozhgan <mozhepeivnad@gmail.com>
2023-01-02 22:47:04 +0330

    create favorite module for jobs (#18)

--- base/src/main/java/com/example/base/routers/AppRouters.kt
+++ base/src/main/java/com/example/base/routers/AppRouters.kt
@@ -1,10 +1,15 @@
 package com.example.base.routers
 
 
-sealed class AppRouters(val routers: String){
-    object AppGraph: AppRouters(routers = "${AppModuleNames.MODULE_BUILDER}://builderGraph")
-    object JobGraph: AppRouters(routers = "${AppModuleNames.MODULE_UI_JOB}://jobGraph")
-    object UserGraph: AppRouters(routers = "${AppModuleNames.MODULE_UI_USER}://userGraph")
-    object JobScreen: AppRouters(routers = "${AppModuleNames.MODULE_UI_JOB}://jobScreen")
-    object UserScreen: AppRouters(routers = "${AppModuleNames.MODULE_UI_USER}://userScreen")
+sealed class AppRouters(val routers: String) {
+    object AppGraph : AppRouters(routers = "${AppModuleNames.MODULE_BUILDER}://builderGraph")
+    object JobGraph : AppRouters(routers = "${AppModuleNames.MODULE_UI_JOB}://jobGraph")
+    object UserGraph : AppRouters(routers = "${AppModuleNames.MODULE_UI_USER}://userGraph")
+    object JobScreen : AppRouters(routers = "${AppModuleNames.MODULE_UI_JOB}://jobScreen")
+    object UserScreen : AppRouters(routers = "${AppModuleNames.MODULE_UI_USER}://userScreen")
+    object FavoriteGraph :
+        AppRouters(routers = "${AppModuleNames.MODULE_UI_FAVORITE}://favoriteGraph")
+
+    object FavoriteScreen :
+        AppRouters(routers = "${AppModuleNames.MODULE_UI_FAVORITE}://favoriteScreen")
 }
---
+++ domain-favorite/src/main/java/com/example/domain_favorite/MyClass.kt
@@ -0,0 +1,4 @@
+package com.example.domain_favorite
+
+class MyClass {
+}
