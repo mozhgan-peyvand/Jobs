9ad0d5287583e348a015738fae4a9269fae0fe59
mozhgan <mozhepeivnad@gmail.com>
2023-01-08 01:41:32 +0330

    create detailJobScreen and scroll animation(#32)

---
+++ ui-jobs/src/main/java/com/example/ui_jobs/screens/JobDetailScreen.kt
@@ -0,0 +1,15 @@
+package com.example.ui_jobs.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun JobDetailScreen() {
+    Column(modifier = Modifier.fillMaxSize().background(Color.Yellow)) {
+
+    }
+}
